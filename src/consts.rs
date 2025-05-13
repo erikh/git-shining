9f1e2be17b02166cae4df3d976072e00d9c717d7
Erik Hollensbe <git@hollensbe.org>
2025-05-13 11:58:03 -0700

    tuned font size a bit; fixed an off by one
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/consts.rs
+++ src/consts.rs
@@ -2,4 +2,4 @@ pub const WEEKS: usize = 53;
 pub const DAYS: usize = 7;
 pub const GRID_SIZE: usize = WEEKS * DAYS;
 pub const PIXEL_HEIGHT: usize = DAYS;
-pub const PIXEL_WIDTH: usize = 15;
+pub const PIXEL_WIDTH: usize = 13;
